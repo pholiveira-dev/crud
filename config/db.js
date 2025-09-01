db146ba2ecb3e5b98355c9acf5aaaf5fdeae1293
Pedro <pedro.alves@escs.edu.br>
2025-09-01 19:03:55 -0300

    ajuste no db.js (problema ao criar a tabela)

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
+// config/db.js
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -5,32 +7,4 @@ const pool = new Pool({
     ssl: { rejectUnauthorized: false }
 });
 
-
-
-const createTable = async () => {
-  try {
-    console.log('Iniciando conexão para criar a tabela...');
-    const client = await pool.connect();
-
-    const query = `
-      CREATE TABLE IF NOT EXISTS produto (
-        id_produto SERIAL PRIMARY KEY,
-        nome VARCHAR(255) NOT NULL,
-        preco DECIMAL(10, 2) NOT NULL,
-        quantidade INT NOT NULL,
-        descricao TEXT
-      );
-    `;
-
-    await client.query(query);
-    console.log('Tabela "produto" criada com sucesso (ou já existia).');
-    client.release();
-  } catch (err) {
-    console.error('Erro ao criar a tabela:', err.message);
-    process.exit(1); // Sai do processo com erro
-  } finally {
-    await pool.end();
-  }
-};
-
-module.exports = { pool, createTable};
+module.exports = pool;
